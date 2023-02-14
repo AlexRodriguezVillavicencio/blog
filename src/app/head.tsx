0c054603472e517412aa01edb0fac95615a9864d
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 <arodriguezv@uni.pe>
2023-02-13 22:00:04 -0500

    typescript project init

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Means – Portfolio Projects</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Proyectos para portafolio" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
